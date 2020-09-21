7791ccfa238651e0859a6af1cd38e9dcb4559a53
Willyan Luindayk <willyanluindayk@gmail.com>
2020-09-21 02:44:12 -0300

    https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if (isset($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'HTTPS') {
+        if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
 
